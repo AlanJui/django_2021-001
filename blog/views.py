b2b20bf2a14452456fb6b1723217a401f3dc10b2
Alan Jui <alanjui.1960@gmail.com>
2021-07-31 21:00:16 +0800

    完成「部落格」功能：（1）條列清單；（2）顯示某篇文章內容。

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 
-# Create your views here.
+from .models import BlogArticles
+
+
+def blog_title(request):
+    blogs = BlogArticles.objects.all()
+    return render(request,
+                  'blog/titles.html',
+                  {
+                      "blogs": blogs
+                  })
+
+def blog_article(request, article_id):
+    # article = BlogArticles.objects.get(id=article_id)
+    article = get_object_or_404(BlogArticles, id=article_id)
+    publish_date = article.publish
+    return render(request,
+                  "blog/content.html",
+                  {
+                      "article": article,
+                      "publish": publish_date
+                  })
